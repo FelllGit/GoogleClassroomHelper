19801ba962a6744832898e386e0545ec88666abd
Данил Палиенко <danilpalienko@MacBook-Pro-Danil.local>
2023-01-26 11:05:40 +0200

    Edit settings

--- src/pages/settings.jsx
+++ src/pages/settings.jsx
@@ -10,7 +10,7 @@ const Settings = ({ pc }) => {
                 <p>Operating System: {pc.os}</p>
                 <p>Processor: {pc.cpu}</p>
                 <p>Processor Architecture: {pc.architecture}</p>
-                <p>Total Memory: {pc.memory}</p>
+                <p>Total Memory: {pc.memory} GB</p>
             </div>
         </>
     );
